79c79ee9e974a2091f3d92621cbb53851006a569
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-01-26 20:20:34 -0300

    feat: add real score

--- src/pages/Score.tsx
+++ src/pages/Score.tsx
@@ -1,19 +1,25 @@
 import { useNavigate } from "react-router";
 import { Button } from "../components/Button";
+import { useStore } from "../store";
 
 export function Score() {
   const navigate = useNavigate();
+  const { correctCount, resetStore: resetCounts } = useStore();
 
   const handleGenerateNewQuestionsClick = () => {
+    resetCounts();
     console.log("generate");
   };
   const handleBackToHomeClick = () => {
+    resetCounts();
     navigate("/");
   };
 
   return (
     <div className="min-h-svh flex flex-col items-center justify-center">
-      <h1 className="font-medium text-[2.5rem]">Você acertou 4/5 🎉</h1>
+      <h1 className="font-medium text-[2.5rem]">
+        Você acertou {correctCount}/5 🎉
+      </h1>
       <div className="flex gap-x-6">
         <Button
           text="Gerar mais questões"
