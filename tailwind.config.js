de12668074bc5e516086f172a28e8110802b965c
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-01-26 01:43:44 -0300

    feat: configure tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: "#DF2935",
+        green: "#0CF574",
+        cyan: {
+          400: "#38BDF8",
+          500: "#0EA5E9",
+          800: "#3B82F6",
+        },
+      },
+    },
   },
   plugins: [],
 };
